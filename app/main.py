d9a0bba4a3030e2b96d6e2d373076625c2d87701
mohammad serpoush <mohammad.serpush@gmail.com>
2023-03-08 11:27:48 +0330

    change

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-import sqltap
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.api_v1.api import api_router
@@ -16,14 +15,6 @@ app = FastAPI(
     redoc_url= None if settings.ENVIRONMENT == "prod" else "/redoc",
 )
 
-
-@app.middleware("http")
-async def add_sql_tap(request: Request, call_next):
-    profiler = sqltap.start()
-    response = await call_next(request)
-    statistics = profiler.collect()
-    sqltap.report(statistics, "report.txt", report_format="text")
-    return response
 
 
 app.add_middleware(
